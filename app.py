487971619c93cc92ceea505547786fae0d6f1711
Metul <metulprabhu@gmail.com>
2023-07-08 11:24:45 +0530

    removed flask forms

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, redirect, url_for,request
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
 from wtforms.validators import NumberRange
@@ -30,9 +30,9 @@ def return_prediction(model,user_input):
     bmi = user_input['bmi']
     HbA1c_level= user_input['HbA1c_level']
     blood_glucose_level= user_input['blood_glucose_level']
-    input = [[gender,age,hypertension,heart_disease,smoking_history,bmi,HbA1c_level,blood_glucose_level]]
+    u_input = [[gender,age,hypertension,heart_disease,smoking_history,bmi,HbA1c_level,blood_glucose_level]]
     #classes = np.array(['setosa', 'versicolor', 'virginica'])
-    output = model.predict_classes(input)
+    output = model.predict_classes(u_input)
     if output>0.1:
         return "you have diabetes"
     else:
@@ -40,8 +40,8 @@ def return_prediction(model,user_input):
 
 app = Flask(__name__)
 # Configure a secret SECRET_KEY
-app.config['SECRET_KEY'] = '6c6722beeac20a0d45f7e977'
-CSRFProtect(app)
+#app.config['SECRET_KEY'] = '6c6722beeac20a0d45f7e977'
+#CSRFProtect(app)
 # Loading the model and scaler
 predictor = load_model('models\\diabetes_predictor')
 #flower_scaler = joblib.load(“iris_scaler.pkl”)
@@ -49,50 +49,46 @@ predictor = load_model('models\\diabetes_predictor')
 
 #gender age	hypertension heart_disease smoking_history bmi HbA1c_level blood_glucose_level diabetes
 
-class DiabetesForm(FlaskForm):
-    gender= StringField('gender')
-    age = StringField('age')
-    hypertension = StringField('hypertension')
-    heart_disease = StringField('heart_disease')
-    smoking_history = StringField('smoking_history')
-    bmi = StringField('bmi')
-    HbA1c_level = StringField('HbA1c_level')
-    blood_glucose_level = StringField('blood_glucose_level')
-    submit = SubmitField('Analyze')
- 
+# class DiabetesForm(FlaskForm):
+#     gender= StringField('gender')
+#     age = StringField('age')
+#     hypertension = StringField('hypertension')
+#     heart_disease = StringField('heart_disease')
+#     smoking_history = StringField('smoking_history')
+#     bmi = StringField('bmi')
+#     HbA1c_level = StringField('HbA1c_level')
+#     blood_glucose_level = StringField('blood_glucose_level')
+#     submit = SubmitField('Analyze')
+u_input={}
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = DiabetesForm()
-    if form.is_submitted():
-        print("submitted")
-
-    if form.validate():
-        print("valid")
-
-    print(form.errors)
-    if form.validate_on_submit():
-        session['gender'] = form.gender.data
-        session['age'] = form.age.data
-        session['hypertension'] = form.hypertension.data
-        session['heart_disease'] = form.heart_disease.data  
-        session['smoking_history'] = form.smoking_history.data 
-        session['bmi'] = form.bmi.data 
-        session['HbA1c_level'] = form.HbA1c_level.data 
-        session['blood_glucose_level'] = form.blood_glucose_level.data 
+    global u_input
+    if request.method=="POST":
+        u_input['gender'] = request.form.get("gender")
+        u_input['age'] = request.form.get("age")
+        u_input['hypertension'] = request.form.get("hypertension")
+        u_input['heart_disease'] = request.form.get("heart_disease")  
+        u_input['smoking_history'] = request.form.get("smoking_history")
+        u_input['bmi'] = request.form.get("bmi")
+        u_input['HbA1c_level'] = request.form.get("HbA1c_level")
+        u_input['blood_glucose_level'] = request.form.get("blood_glucose_level")
+        print("a")
         return redirect(url_for('prediction'))
         #prediction()
-    return render_template('index.html', form=form)
+    return render_template('index.html')
 @app.route('/prediction')
 def prediction():
+    global u_input
     user_input = {}
-    user_input['gender'] = str(session['gender'])
-    user_input['age'] = float(session['age'])
-    user_input['hypertension'] = int(session['hypertension'])
-    user_input['heart_disease'] = int(session['heart_disease'])
-    user_input['smoking_history'] = str(session['smoking_history'])
-    user_input['bmi'] = float(session['bmi'])
-    user_input['HbA1c_level'] = float(session['HbA1c_level'])
-    user_input['blood_glucose_level'] = int(session['blood_glucose_level'])
+    user_input['gender'] = str(u_input['gender'])
+    user_input['age'] = float(u_input['age'])
+    user_input['hypertension'] = int(u_input['hypertension'])
+    user_input['heart_disease'] = int(u_input['heart_disease'])
+    user_input['smoking_history'] = str(u_input['smoking_history'])
+    user_input['bmi'] = float(u_input['bmi'])
+    user_input['HbA1c_level'] = float(u_input['HbA1c_level'])
+    user_input['blood_glucose_level'] = int(u_input['blood_glucose_level'])
+    print(user_input)
     results = return_prediction(predictor,user_input)
     return render_template('prediction.html',results=results)
 
