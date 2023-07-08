dfd2dc80c5b33c0057598ff6cd655138ebc71664
Metul <metulprabhu@gmail.com>
2023-07-08 10:36:59 +0530

    modified csrf commands

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from wtforms.validators import NumberRange
 import numpy as np 
 from keras.models import load_model
 from sklearn.preprocessing import LabelEncoder
+from flask_wtf.csrf import CSRFProtect
+
 #parameters:
 #gender age	hypertension heart_disease smoking_history bmi HbA1c_level blood_glucose_level diabetes
 def return_prediction(model,user_input):    
@@ -38,7 +40,8 @@ def return_prediction(model,user_input):
 
 app = Flask(__name__)
 # Configure a secret SECRET_KEY
-app.config['SECRET_KEY'] = 'ed6526783adfa9542651afe3'
+app.config['SECRET_KEY'] = '6c6722beeac20a0d45f7e977'
+CSRFProtect(app)
 # Loading the model and scaler
 predictor = load_model('models\\diabetes_predictor')
 #flower_scaler = joblib.load(“iris_scaler.pkl”)
@@ -60,6 +63,13 @@ class DiabetesForm(FlaskForm):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = DiabetesForm()
+    if form.is_submitted():
+        print("submitted")
+
+    if form.validate():
+        print("valid")
+
+    print(form.errors)
     if form.validate_on_submit():
         session['gender'] = form.gender.data
         session['age'] = form.age.data
@@ -69,21 +79,22 @@ def index():
         session['bmi'] = form.bmi.data 
         session['HbA1c_level'] = form.HbA1c_level.data 
         session['blood_glucose_level'] = form.blood_glucose_level.data 
-        return redirect(url_for('prediction.html'))
+        return redirect(url_for('prediction'))
+        #prediction()
     return render_template('index.html', form=form)
 @app.route('/prediction')
 def prediction():
     user_input = {}
-    user_input['gender'] = session['gender']
+    user_input['gender'] = str(session['gender'])
     user_input['age'] = float(session['age'])
     user_input['hypertension'] = int(session['hypertension'])
     user_input['heart_disease'] = int(session['heart_disease'])
-    user_input['smoking_history'] = session['smoking_history']
+    user_input['smoking_history'] = str(session['smoking_history'])
     user_input['bmi'] = float(session['bmi'])
     user_input['HbA1c_level'] = float(session['HbA1c_level'])
     user_input['blood_glucose_level'] = int(session['blood_glucose_level'])
     results = return_prediction(predictor,user_input)
-    return render_template('prediction.html',results)
+    return render_template('prediction.html',results=results)
 
 if __name__ == '__main__':
     app.run(debug=True)
