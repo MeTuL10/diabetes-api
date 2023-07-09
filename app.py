dc0c142aa7ca89cdf37d6cb0fafdc5a900bf990c
Metul <metulprabhu@gmail.com>
2023-07-09 17:22:51 +0530

    changed port

--- app.py
+++ app.py
@@ -100,4 +100,4 @@ def prediction():
     return render_template('prediction.html',results=results)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8000)
+    app.run(host='0.0.0.0',port=5000)
